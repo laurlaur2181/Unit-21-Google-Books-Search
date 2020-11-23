fde67179c7b92948800f1541681da4e72804080c
Lauren Goettsch <lgoettsch2181@gmail.com>
2020-11-22 19:07:51 -0800

    books

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
   {
@@ -25,7 +23,6 @@ mongoose.connect(
   }
 );
 
-// Start the API server
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
 );
